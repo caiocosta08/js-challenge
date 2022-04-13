38b0d19f4d75da8e0b26a54c8212c21716f1e1fc
caiocostaee@gmail.com <caiocostaee@gmail.com>
2022-04-13 19:16:49 -0300

    third commit

--- problem2.js
+++ problem2.js
@@ -12,9 +12,9 @@ let results1 = array1.map( item => squared(item))
 let results2 = array2.map( item => squared(item))
 let results3 = array3.map( item => squared(item))
 
-results1.sort((x,y => x - y))
-results2.sort((x,y => x - y))
-results3.sort((x,y => x - y))
+results1.sort((x,y) => x - y)
+results2.sort((x,y) => x - y)
+results3.sort((x,y) => x - y)
 
 console.log(correct1, results1, )
 console.log(correct2, results2, )
